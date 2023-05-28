5d41cdf4bba4665f3522d3f93cbcd7666e6f8cf5
Muhammad Arbaz <arbazdogar20@gmail.com>
2023-05-28 11:13:16 +0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express');
+const multer = require('multer');
+const app = express();
+const fs = require('fs');
+
+const cloudinary = require('./utils/cloudinaryUtils');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Directory to store uploaded files
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Use original file name
+  },
+});
+
+// const upload = multer({storage: multer.memoryStorage()});
+const upload = multer({storage: storage});
+
+app.post('/upload', upload.single('fileName'), async (req, res) => {
+  const file = req.file.path;
+
+  try {
+    const fileUpload = await cloudinary.uploader.upload(file, {
+      resource_type: 'auto',
+      public_id: `${req.file.originalname}`,
+      folder: 'Files',
+    });
+    if (fileUpload) {
+      fs.unlinkSync(file);
+      return res.status(200).json(fileUpload.secure_url);
+    }
+  } catch (err) {
+    let statusCode = err.http_code || 500;
+    return res.status(statusCode).json(err);
+  }
+});
+
+app.listen(5000, (req, res) => {
+  console.log('Server Is Ruuning');
+});
